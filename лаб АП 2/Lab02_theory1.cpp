c240b5ef2fb640921b6362c321368934cae41294
Maria <mariia.pysachuk.ik.2022@lpnu.ua>
2022-10-06 22:45:24 +0300

    first master commit

---
+++ лаб АП 2/Lab02_theory1.cpp
@@ -0,0 +1,30 @@
+// Lab_02.cpp
+// < Писачук Марія >
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 13
+
+#include <iostream> 
+#include <cmath> 
+using namespace std;
+int main()
+
+{
+	double a; // вхідний параметр 
+	double b; // вхідний параметр 
+	double z1; // результат обчислення 1-го виразу 
+	double z2; // результат обчислення 2-го виразу 
+
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+
+	z1 = (sin(a) + cos((2 * b - a)) / (cos(a) - sin(2 * b - a)));
+	z2 = 1 + sin(2 * b) / cos(2 * b);
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+
+	return 0;
+}
